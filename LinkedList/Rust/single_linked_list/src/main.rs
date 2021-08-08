2afb0cb92fbe24c1d9ff9e1a86dd64a997ce1fa4
roach <tmdgusya@naver.com>
2021-08-08 22:43:26 +0900

    test: create SingleLinkedList create test

--- LinkedList/Rust/single_linked_list/src/main.rs
+++ LinkedList/Rust/single_linked_list/src/main.rs
@@ -1,3 +1,22 @@
+mod single_linked_list;
+use single_linked_list::SingleLinkedList;
+use single_linked_list::Node;
+
 fn main() {
-    println!("Hello, world!");
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn create_linked_list_test() {
+        let node = Node {data: 10, next: None};
+        let linked_list = SingleLinkedList {head: Some(node)};
+        assert_eq! (linked_list.head.unwrap().data, 10)
+    }
+
+
 }

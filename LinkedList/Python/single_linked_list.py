84e76ccbc0beb6277374afc8b81edceed5d04eb6
roach <tmdgusya@naver.com>
2021-08-08 16:09:13 +0900

    feat: add isExist func
    isExist func have TimeComplexity O(n)

--- LinkedList/Python/single_linked_list.py
+++ LinkedList/Python/single_linked_list.py
@@ -14,3 +14,15 @@ class SingleLinkedList(object):
             while cur.next != None:
                 cur = cur.next
             cur.next = node
+
+    # O(n) Time Complexity
+    def isExist(self, node):
+        
+        if self.head == None:
+            return False
+        else:
+            cur = self.head;
+            while cur.next != None:
+                if cur == node:
+                    return True
+            return False

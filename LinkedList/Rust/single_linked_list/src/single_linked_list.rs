f8e8ee73730ffc337c5c378b837b9123eeafc3d4
roach <tmdgusya@naver.com>
2021-08-09 00:45:28 +0900

    feat: impl Node counstructor

--- LinkedList/Rust/single_linked_list/src/single_linked_list.rs
+++ LinkedList/Rust/single_linked_list/src/single_linked_list.rs
@@ -1,6 +1,12 @@
 pub struct Node<T> {
     pub data: T,
-    pub next: Option<Box<Node<T>>>
+    pub next: Option<NonNull<Node<T>>>
+}
+
+impl<T> Node<T> {
+    pub fn new(data: T) -> Node<T> {
+        Node { data: data, next: None }
+    }
 }
 
 pub struct SingleLinkedList<T> {

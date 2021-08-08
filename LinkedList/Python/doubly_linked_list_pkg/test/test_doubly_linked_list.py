736999bea8a02772d85a78b64f1974d82c56a530
roach <tmdgusya@naver.com>
2021-08-09 00:36:44 +0900

    test: test append front if exist head node

--- LinkedList/Python/doubly_linked_list_pkg/test/test_doubly_linked_list.py
+++ LinkedList/Python/doubly_linked_list_pkg/test/test_doubly_linked_list.py
@@ -10,3 +10,13 @@ class DoublyLinkedListTest(unittest.TestCase):
         doublyLinkedList.append_front(node)
 
         self.assertEqual(doublyLinkedList.head, node);
+
+    def test_append_front_if_exist_head_node(self):
+        doubly_linked_list = DoublyLinkedList()
+        head_node = Node("head")
+        new_node = Node("My Position")
+        doubly_linked_list.append_front(head_node)
+        doubly_linked_list.append_front(new_node)
+
+        self.assertEqual(doubly_linked_list.head, new_node)
+        self.assertEqual(new_node.next, head_node)

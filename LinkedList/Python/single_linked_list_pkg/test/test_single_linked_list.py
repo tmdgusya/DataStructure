d0ef77193af9ec9eaef299e1081ac4279ddcbbcf
roach <tmdgusya@naver.com>
2021-08-08 17:02:50 +0900

    test: add success remove node test
    return true if success remove node

--- LinkedList/Python/single_linked_list_pkg/test/test_single_linked_list.py
+++ LinkedList/Python/single_linked_list_pkg/test/test_single_linked_list.py
@@ -4,6 +4,9 @@ from single_linked_list_pkg.single_linked_list import SingleLinkedList
 
 class SingleLinkedListTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        print("IN TEST METHOD", self._testMethodName)
+
 
     def test_append_func(self):
         linkedList = SingleLinkedList()
@@ -12,3 +15,11 @@ class SingleLinkedListTest(unittest.TestCase):
 
         linkedList.append(node_one);
         self.assertEqual(linkedList.head, node_one);
+
+    def test_return_ture_if_success_remove_node_in_linked_list(self):
+        linkedList = SingleLinkedList()
+
+        node_one = Node("header")
+
+        linkedList.append(node_one);
+        self.assertTrue(linkedList.remove_node(node_one));

1339659ac0d9370b178b8508033bdc4a61895ea4
roach <tmdgusya@naver.com>
2021-08-16 21:32:37 +0900

    traveling head, tail

--- LinkedList/Python/doubly_linked_list_pkg/doubly_linked_list.py
+++ LinkedList/Python/doubly_linked_list_pkg/doubly_linked_list.py
@@ -7,7 +7,30 @@ class DoublyLinkedList(object):
     def append_front(self, node):
         if self.head == None:
             self.head = node
+        if self.tail == None:
+            self.tail = node
         else:
+            self.head.prev = node
             node.next = self.head
             self.head = node
             node.prev = None
+
+    def traversing_front(self, target):
+        node = self.head
+        count = 0;
+        while node != None:
+            if node == target:
+                return count
+            node = node.next
+            count = count + 1
+        return -1
+
+    def traversing_tail(self, target):
+        node = self.tail
+        count = 0;
+        while node != None:
+            if node == target:
+                return count
+            node = node.prev
+            count = count + 1
+        return -1
--- LinkedList/Python/doubly_linked_list_pkg/test/test_doubly_linked_list.py
+++ LinkedList/Python/doubly_linked_list_pkg/test/test_doubly_linked_list.py
@@ -20,3 +20,21 @@ class DoublyLinkedListTest(unittest.TestCase):
 
         self.assertEqual(doubly_linked_list.head, new_node)
         self.assertEqual(new_node.next, head_node)
+
+    def test_search_front(self):
+        doubly_linked_list = DoublyLinkedList() ;
+        target_node = Node("zzzz");
+        doubly_linked_list.append_front(Node("head"));
+        doubly_linked_list.append_front(Node("My Position"));
+        doubly_linked_list.append_front(target_node);
+        doubly_linked_list.append_front(Node("12323"));
+        self.assertEqual(doubly_linked_list.traversing_front(target_node), 1);
+    
+    def test_search_tail(self):
+        doubly_linked_list = DoublyLinkedList() ;
+        target_node = Node("zzzz");
+        doubly_linked_list.append_front(Node("head"));
+        doubly_linked_list.append_front(Node("My Position"));
+        doubly_linked_list.append_front(target_node);
+        doubly_linked_list.append_front(Node("12323"));
+        self.assertEqual(doubly_linked_list.traversing_tail(target_node), 2);
